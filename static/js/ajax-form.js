af45ec96a15ddc40f916336f65bd301de38e6f35
hdevilbiss <hannahdevilbiss@gmail.com>
2020-11-22 17:36:58 -0500

    Add comments to ajax-form.js

--- static/js/ajax-form.js
+++ static/js/ajax-form.js
@@ -6,20 +6,27 @@ window.addEventListener("DOMContentLoaded", function() {
     var button = document.getElementById("my-form-button");
     var status = document.getElementById("my-form-status");
 
-    // Success and Error functions for after the form is submitted
-
+    /**
+     * success
+     * This receives two parameters from ajax(), but doesn't use them?
+     */
     function success() {
       form.reset();
       button.style = "display: none ";
       status.innerHTML = "Thanks!";
     }
 
+    /**
+     * success
+     * This receives two parameters from ajax(), but doesn't use them?
+     */
     function error() {
       status.innerHTML = "Oops! There was a problem.";
     }
 
-    // handle the form submission event
-
+    /**
+     * Event Handler: form submission
+     */
     form.addEventListener("submit", function(ev) {
       ev.preventDefault();
       var data = new FormData(form);
@@ -27,19 +34,26 @@ window.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-// helper function for sending an AJAX request
-
+/**
+ * helper function: ajax
+ * @param {*} method
+ * @param {*} url
+ * @param {*} data
+ * @param {function} success what to do if HTTP 200
+ * @param {*} error
+ * @return {void} : Open, set, check, and send with XHR
+ */
 function ajax(method, url, data, success, error) {
-var xhr = new XMLHttpRequest();
-xhr.open(method, url);
-xhr.setRequestHeader("Accept", "application/json");
-xhr.onreadystatechange = function() {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-    success(xhr.response, xhr.responseType);
-    } else {
-    error(xhr.status, xhr.response, xhr.responseType);
-    }
-};
-xhr.send(data);
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
 }
